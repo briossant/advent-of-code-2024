08e450f44daeb8d64d9ebd0eb55b79564f521dda
Crosson Brieuc <crosson.brieuc@gmail.com>
2024-12-06 20:38:33 +0900

    day-5 part 2 had enough of debugging, I will try a dummy algorithm

--- day-6/guard-2.py
+++ day-6/guard-2.py
@@ -12,11 +12,11 @@ for i in range(h):
     if lines[i].count('^'):
         x = lines[i].index('^')
         y = i
+        lines[y][x] = 'X'
         break
 
 
 d = 0
-c = ['^', '>', 'v', '<']
 
 
 def move(x, y, d):
@@ -36,35 +36,56 @@ def out_of_bounds(x, y):
     return x < 0 or y < 0 or x >= w or y >= h
 
 
-loops = 0
+def check_loop(X, Y, D):
+    loop = False
+    to_add = []
+    nd = (D+1) % 4
+    stx, sty = move(X, Y, D)
+
+    if lines[sty][stx].count('#'):
+        raise ValueError('Non non')
+    if lines[sty][stx] == 'O':
+        return
+
+    lines[sty][stx] = '#' + str(D)
+    while True:
+        nx, ny = move(X, Y, nd)
+        if out_of_bounds(nx, ny):
+            break
+        if lines[ny][nx].count('#'):
+            if lines[ny][nx].count(str(nd)) or [nx, ny, nd] in to_add:
+                print(stx, sty, D)
+                loop = True
+                break
+            to_add.append([nx, ny, nd])
+            nd = (nd+1) % 4
+        else:
+            X, Y = nx, ny
+    lines[sty][stx] = 'O' if loop else '.'
+
+
+c = ['^', '>', 'v', '<']
 while True:
     nx, ny = move(x, y, d)
-    nd = (d+1) % 4
     if out_of_bounds(nx, ny):
         break
     if lines[ny][nx].count('#'):
         lines[ny][nx] += str(d)
-        d = nd
+        d = (d+1) % 4
     else:
+        if lines[y][x] != 'O':
+            lines[y][x] = c[d]
+        check_loop(x, y, d)
         x, y = nx, ny
-        to_add = []
-        while True:
-            nnx, nny = move(nx, ny, nd)
-            if out_of_bounds(nnx, nny):
-                nx, ny = nnx, nny
-                break
-            if lines[nny][nnx].count('#'):
-                if lines[nny][nnx].count(str(nd)) or [nnx, nny, nd] in to_add:
-                    loops += 1
-                    break
-                to_add.append([nnx, nny, nd])
-                nd = (nd+1) % 4
-            else:
-                nx, ny = nnx, nny
-        if not out_of_bounds(nx, ny):
-            lines[y][x] = 'O'
-            for rx, ry, rd in to_add:
-                lines[ry][rx] += str(rd)
 
-print("\n".join(["".join(l) for l in lines]))
-print("part 2, number of loops: "+str(loops))
+
+def rep(x):
+    if x.count('#'):
+        return '#'
+    return x
+
+
+print("\n".join(["".join(map(rep, l)) for l in lines]))
+res = sum([l.count('O') for l in lines])
+
+print("part 2, number of loops: "+str(res))
