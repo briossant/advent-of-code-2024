66203ea1494e39150de6477bb0918f9f99c2207c
Crosson Brieuc <crosson.brieuc@gmail.com>
2024-12-16 15:38:28 +0900

    day-16 examples are working

--- day-16/part-2.py
+++ day-16/part-2.py
@@ -1,5 +1,4 @@
 import re
-import queue
 import functools as ft
 import sys
 
@@ -20,7 +19,6 @@ for x in range(w):
         if Map[y][x] == 'S':
             st_x = x
             st_y = y
-            Map[y][x] = 0
         if Map[y][x] == 'E':
             ed_x = x
             ed_y = y
@@ -29,37 +27,38 @@ for x in range(w):
 dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
-def run():
-    q = queue.Queue()
-    q.put((st_x, st_y, 1, 0))
-    while not q.empty():
-        x, y, last_d, cost = q.get()
+def run(x, y, last_d, cost):
+    if Map[y][x] == '#':
+        return
+    if Map[y][x] == 'E':
+        return
+    if Map[y][x] == '.' or Map[y][x] == 'S' or Map[y][x] > cost:
+        Map[y][x] = cost
+    elif Map[y][x] != '.' and Map[y][x] != 'S' and Map[y][x] < cost:
+        return
 
-        d = 0
-        for (dx, dy) in dirs:
-            nx, ny = x+dx, y+dy
-            nc = cost + (1 if d == last_d else 1001)
-            if Map[ny][nx] == '#' or Map[ny][nx] == 'E':
-                continue
-            if Map[ny][nx] == '.' or Map[ny][nx] > nc:
-                Map[ny][nx] = nc
-            elif Map[ny][nx] != '.' and Map[ny][nx] <= nc:
-                continue
-            q.put((nx, ny, d, nc))
+    d = 0
+    for (dx, dy) in dirs:
+        if (d+2) % 4 == last_d:
             d += 1
+            continue
+        nx, ny = x+dx, y+dy
+        nc = cost + (1 if d == last_d else 1001)
+        run(nx, ny, d, nc)
+        d += 1
 
 
-def backrun(x, y, cost):
+def backrun(x, y, cost, last_c):
     Map[y][x] = 'O'
     for (dx, dy) in dirs:
         ny, nx = y+dy, x+dx
         if Map[ny][nx] != '#' and Map[ny][nx] != '.' and Map[ny][nx] != 'O':
-            if Map[ny][nx] < cost:
-                backrun(nx, ny, Map[ny][nx])
+            if Map[ny][nx] < cost or ((Map[ny][nx] == last_c-1002 or Map[ny][nx] == last_c-2) and last_c != -1):
+                backrun(nx, ny, Map[ny][nx], cost)
 
 
-run()
-print(Map)
+run(st_x, st_y, 1, 0)
+
 
 ends = [Map[ed_y+dy][ed_x+dx] for dx, dy in dirs]
 res = 10000000000000
@@ -71,7 +70,9 @@ Map[ed_y][ed_x] = res
 print('\n'.join(
     [''.join([format(x, '05d') if x != 'O' and x != '#' else x+'====' for x in l]) for l in Map]))
 
-backrun(ed_x, ed_y, Map[ed_y][ed_x])
+print("res: " + str(res))
+
+backrun(ed_x, ed_y, Map[ed_y][ed_x], -1)
 
 print('\n'.join(
     [''.join(['.' if x != 'O' and x != '#' else x for x in l]) for l in Map]))
