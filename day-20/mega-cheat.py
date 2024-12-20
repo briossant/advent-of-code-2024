b1d5c2cf331ba1f6429572ddd087053f331cda10
Crosson Brieuc <crosson.brieuc@gmail.com>
2024-12-20 16:30:41 +0900

    day-20 example is working but not the inputs

--- day-20/mega-cheat.py
+++ day-20/mega-cheat.py
@@ -9,27 +9,21 @@ NormalRace = MapCopy(Map)
 BFS2(NormalRace, 'S', 'E')
 
 
-def getDiff(CheatPath, st_time):
+def getDiff(x, y, offset):
     diffs = []
-    ends = []
-    for x in range(w):
-        for y in range(h):
-            if type(CheatPath[y][x]) is not int:
-                continue
+    for dx in range(-offset, offset):
+        dy = abs(dx)-offset
+        nx, ny = x+dx, y+dy
 
-            for dx, dy in cross_dirs:
-                nx, ny = x+dx, y+dy
-                if MapIsOutOfBounds(Map, nx, ny) or type(NormalRace[ny][nx]) is not int:
-                    continue
+        if not MapIsOutOfBounds(Map, nx, ny) and type(NormalRace[ny][nx]) is int:
+            d = NormalRace[ny][nx] - offset - NormalRace[y][x] - 1
+            diffs.append(d + d % 2)
 
-                d = NormalRace[ny][nx] - st_time - CheatPath[y][x] - 1
-                if (nx, ny) in ends:
-                    i = ends.index((nx, ny))
-                    if diffs[i] < d:
-                        diffs[i] = d
-                else:
-                    diffs.append(d)
-                    ends.append((nx, ny))
+        dy = -dy
+        nx, ny = x+dx, y+dy
+        if dy != 0 and not MapIsOutOfBounds(Map, nx, ny) and type(NormalRace[ny][nx]) is int:
+            d = NormalRace[ny][nx] - offset - NormalRace[y][x]
+            diffs.append(d + d % 2)
     return diffs
 
 
@@ -42,16 +36,15 @@ for x in range(1, w-1):
     for y in range(1, h-1):
         if Map[y][x] == '#':
             continue
-        CheatPath = MapCopy(Map)
-        BFS(CheatPath, x, y, gdchars=['#', '.'], max_n=20)
-        diffs = getDiff(CheatPath, NormalRace[y][x])
-        for diff in diffs:
-            if diff >= MIN_SAVE:
-                if diff in shortcut:
-                    shortcut[diff] += 1
-                else:
-                    shortcut[diff] = 1
-                ttshortcuts += 1
+        for offset in range(1, 21):
+            diffs = getDiff(x, y, offset)
+            for diff in diffs:
+                if diff >= MIN_SAVE:
+                    if diff in shortcut:
+                        shortcut[diff] += 1
+                    else:
+                        shortcut[diff] = 1
+                    ttshortcuts += 1
 
 pprint.pprint(shortcut)
 print("shortcuts:", ttshortcuts)
